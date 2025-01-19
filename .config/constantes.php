c4410516125ab5bf0e5e648fe79ceb00b870c7e3
Kh4yA <87660873+Kh4yA@users.noreply.github.com>
2025-01-19 18:47:27 +0100

    Delete .config directory

--- .config/constantes.php
+++
@@ -1,4 +0,0 @@
-<?php
-
-define('DOC_PATH',dirname( __DIR__).DIRECTORY_SEPARATOR);
-define('VIEW_PATH', dirname(__DIR__) . DIRECTORY_SEPARATOR . 'templates' . DIRECTORY_SEPARATOR);
--- .config/datadb.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    "user_name" => "devserhdevassist",
-    "password" =>"354gHGNB5hnDnl59uj34",
-    "host"=>"devserhdevassist.mysql.db",
-    "name"=>"devassist"
-];
-
